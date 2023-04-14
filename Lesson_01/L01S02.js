0551a14ea142dc46fb854ce26ace31804f4eac5c
Karol Švaral <112489961+Kajoo72@users.noreply.github.com>
2023-04-14 11:07:35 +0200

    Update L01S02.js

--- Lesson_01/L01S02.js
+++ Lesson_01/L01S02.js
@@ -8,3 +8,17 @@ gr.query();
 while (gr.next()) {
     gs.info(gr.getValue('number'));
 }
+
+edit 14.4.2023
+/*
+var gr = new GlideRecord('incident');
+gr.addActiveQuery();
+gr.addQuery("priority", "5")
+gr.query();
+
+while (gr.next()) {
+    gs.info(gr.getValue('number'));
+    
+}
+gs.info("There is " +  gr.getRowCount() + " active Incidents with lowest Priority");
+*/
